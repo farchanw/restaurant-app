93ba26cfe7973a4d60efcfab675c3d4f536d62d5
Farchan Wahyu Ananta <34596157+farhan-wa@users.noreply.github.com>
2022-10-20 15:28:22 +0700

    fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,9 +5,6 @@ import swRegister from './utils/sw-register';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-const START = 10;
-const NUMBER_OF_IMAGES = 100;
-
 const app = new App({
   button: document.querySelectorAll('.hamburger'),
   drawer: document.querySelector('#drawer .navbar-menu'),
--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -2,17 +2,15 @@ import * as WorkboxWindow from 'workbox-window';
 
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
-    console.log('Service Worker not supported in the browser');
-    return;
+    throw new Error('Service Worker not supported in the browser');
   }
 
   const wb = new WorkboxWindow.Workbox('./sw.bundle.js');
 
   try {
     await wb.register();
-    console.log('Service worker registered');
   } catch (error) {
-    console.log('Failed to register service worker', error);
+    throw new Error('Failed to register service worker');
   }
 };
 
